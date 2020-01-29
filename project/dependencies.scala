644b167b732130bcd42af5289a3a66e275de8a99
keiSunagawa <keisunagawa.git@icloud.com>
2020-01-30 00:39:34 +0900

    scala steward check

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Versions {
   lazy val scalaTest = "3.1.0"
-  lazy val cats = "2.1.0"
+  lazy val cats = "2.0.0"
 }
 object Dependencies {
   lazy val testDep = Seq("org.scalatest" %% "scalatest" % Versions.scalaTest)
